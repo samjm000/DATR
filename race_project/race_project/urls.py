84b8b6e78d411870242900a53f2e990e923d0404
Sam McInerney <samjm000@yahoo.co.uk>
2024-12-20 06:41:13 +0000

    pop

--- race_project/race_project/urls.py
+++ race_project/race_project/urls.py
@@ -6,7 +6,11 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("raceapp/", include("raceapp.urls")),
-    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="registration/login.html"),
+        name="login",
+    ),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path(
         "", lambda request: HttpResponseRedirect("/raceapp/")
