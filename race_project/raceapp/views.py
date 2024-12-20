b8b2f4208b9b4fe13b9c654be93d5a7cc85bfdc3
Sam McInerney <samjm000@yahoo.co.uk>
2024-12-20 06:58:00 +0000

    pop

--- race_project/raceapp/views.py
+++ race_project/raceapp/views.py
@@ -8,6 +8,32 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import Racecard, Race, Horse
 from .tasks import scrape_and_save_data
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+    return render(request, "registration/login.html", {"form": form})
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
 
 
 def home(request):
