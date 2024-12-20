f822d3984dfa52026373b673cb31843b85b44a36
Sam McInerney <samjm000@yahoo.co.uk>
2024-12-20 06:51:18 +0000

    oioi

--- race_project/race_project/urls.py
+++ race_project/race_project/urls.py
@@ -1,18 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponseRedirect
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("raceapp/", include("raceapp.urls")),
     path(
-        "login/",
-        auth_views.LoginView.as_view(template_name="registration/login.html"),
-        name="login",
-    ),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+        "raceapp/", include("raceapp.urls")
+    ),  # This includes URLs from the 'raceapp' app
     path(
         "", lambda request: HttpResponseRedirect("/raceapp/")
-    ),  # Ensure the redirect to raceapp
+    ),  # Redirect to 'raceapp' by default
 ]
--- race_project/raceapp/urls.py
+++ race_project/raceapp/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
-from .views import home, run_scraper, save_race_data, display_race_data, get_progress
+from .views import (
+    home,
+    run_scraper,
+    save_race_data,
+    display_race_data,
+    get_progress,
+    login_view,
+    register_view,
+)
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", home, name="home"),
@@ -7,4 +16,7 @@ urlpatterns = [
     path("save_race_data/", save_race_data, name="save_race_data"),
     path("display_race_data/", display_race_data, name="display_race_data"),
     path("get_progress/<str:task_id>/", get_progress, name="get_progress"),
+    path("login/", login_view, name="login"),  # Add login view
+    path("register/", register_view, name="register"),  # Add register view
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),  # Add logout view
 ]
